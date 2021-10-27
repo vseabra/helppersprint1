5934f16a557636677b27f5d1535adea0cc0442be
Vinícius Seabra <vseabra@protonmail.com>
2021-10-27 14:50:46 -0300

    lint

--- src/ex_2/ex_2_funcional.ts
+++ src/ex_2/ex_2_funcional.ts
@@ -1,45 +1,45 @@
-
 interface Person {
-  id: number,
-  name: string,
-  bio: string
+  id: number;
+  name: string;
+  bio: string;
 }
 
 interface Update {
-  name?: string,
-  bio?: string,
+  name?: string;
+  bio?: string;
 }
-
 
 // recebe um id e retorna a bio correspodente.
 const getBioByID = (id: number, list: Array<Person>): string | undefined => {
-  const person = list.find(person => person.id === id)
-  return person ? person.bio : undefined
-}
+  const person = list.find((person) => person.id === id);
+  return person ? person.bio : undefined;
+};
 
 // recebe um id e retorna o nome.
 const getNameByID = (id: number, list: Array<Person>): string | undefined => {
-  const person = list.find(person => person.id === id)
-  return person ? person.name : undefined
-}
+  const person = list.find((person) => person.id === id);
+  return person ? person.name : undefined;
+};
 
-// recebe um id e retorna a lista sem o elemento 
+// recebe um id e retorna a lista sem o elemento
 const deleteByID = (id: number, list: Array<Person>): Array<Person> => {
-  const updatedList = list.filter(person => person.id !== id)
-  return updatedList
-}
+  const updatedList = list.filter((person) => person.id !== id);
+  return updatedList;
+};
 
 // recebe um id e uma mudança {name?: string, bio?: string} e retorna a lista com a mudança aplicada
 const updateByID = (id: number, list: Array<Person>, update: Update): Array<Person> => {
-  const personToUpdate = list.find(object => object.id === id)
-  if (personToUpdate === undefined){
-    return list
+  const personToUpdate = list.find((object) => object.id === id);
+  if (personToUpdate === undefined) {
+    return list;
   }
 
-  const updatedPerson = {...personToUpdate, ... update}
-  const updatedList: Array<Person> = list.map(person => person !== personToUpdate ? person : updatedPerson )
+  const updatedPerson = { ...personToUpdate, ...update };
+  const updatedList: Array<Person> = list.map((person) =>
+    person !== personToUpdate ? person : updatedPerson
+  );
 
-  return updatedList
-}
+  return updatedList;
+};
 
-export {getBioByID, getNameByID, deleteByID, updateByID, Update, Person}
+export { getBioByID, getNameByID, deleteByID, updateByID, Update, Person };
